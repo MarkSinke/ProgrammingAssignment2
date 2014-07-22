afbb5d940a25ff894cc0e92c414470ddffaa5c95
MarkSinke <m.j.sinke@gmail.com>
2014-07-23 00:34:57 +0200

    Fix issue where a different set of arguments to cacheSolve would incorrectly yield the cached result for the previous set of arguments.

--- cachematrix.R
+++ cachematrix.R
@@ -9,14 +9,25 @@
 ## get its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## cached inverse
     inverse <- NULL
+    ## cached arguments used to compute the inverse - if an solution is requested with
+    ## different arguments, the cached inverse will not be returned.
+    arguments <- NULL
+
     set <- function(y) {
         x <<- y
         inverse <<- NULL
+        args <<- NULL
     }
     get <- function() x
-    setInverse <- function(inv) inverse <<- inv
-    getInverse <- function() inverse
+    setInverse <- function(inv, ...) { 
+        inverse <<- inv
+        arguments <<- list(...)
+    }
+    getInverse <- function(...) {
+        if (identical(list(...), arguments)) inverse else NULL
+    }
     
     list(set = set, get = get,
        setInverse = setInverse,
@@ -26,39 +37,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Compute the inverse of the matrix ("solve the matrix")
 ##
-## This function will cache the results of the operation as long as the matrix stays the same.
-## Note that any differences in arguments to the cacheSolve function are not
-## taken into account when caching the solved value. For example, when the cacheSolve
-## function is used a second time to actually solve a set of linear equations, with different right-hand
-## sides of the linear system, instead of the (default) identity matrix, the function
-## will (incorrectly) return the previously cached inverse, instead of the actual solution.
+## This function will cache the results of the last solve operation as long as the matrix
+## stays the same. If the remaining arguments to the cacheSolve function are different
+## from the last call, a new inverse will be computed and stored in the cache.
 ##
-## For example:
-## > m <- makeCacheMatrix(matrix(1:4, 2, 2))
-## > cacheSolve(m)
-##      [,1] [,2]
-## [1,]   -2  1.5
-## [2,]    1 -0.5
-## Now solving the matrix for another right-hand side matrix, like
-## > solve(m$get(), matrix(c(2, 0, 0, 2), 2, 2))
-##      [,1] [,2]
-## [1,]   -4    3
-## [2,]    2   -1
-## but
-## > cacheSolve(m, matrix(c(2,0,0,2), 2, 2))
-## getting cached data
-##      [,1] [,2]
-## [1,]   -2  1.5
-## [2,]    1 -0.5
-## which is clearly incorrect.
+## This fixes the incorrect behavior exhibited in the previous version of this function
+## (see previous commit).
 cacheSolve <- function(x, ...) {
-    m <- x$getInverse()
+    m <- x$getInverse(...)
     if (!is.null(m)) {
         message("getting cached data")
         return(m)
     }
     data <- x$get()
     inverse <- solve(data, ...)
-    x$setInverse(inverse)
+    x$setInverse(inverse, ...)
     inverse
 }
